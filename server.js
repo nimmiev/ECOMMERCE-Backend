855bce10dc3da58450fe7b624dbac58129f5a50c
Nimmi E V <96404178+nimmiev@users.noreply.github.com>
2025-01-20 01:34:35 +0530

    Update server.js
    allow list

--- server.js
+++ server.js
@@ -5,8 +5,10 @@ const app = express()
 const productRouter = require('./src/routes/productRouter')
 const userRouter = require('./src/routes/userRouter')
 const ProductModel = require('./src/models/productModel')
-const port = process.env.PORT || 3000
 const cors = require('cors')
+const port = process.env.PORT || 3000
+const list = process.env.ALLOWLIST || 'https://ecommerce-frontend-theta-flax.vercel.app';
+
 
 async function main() {
     await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -19,13 +21,15 @@ main()
 .catch(err => console.log(err))
 
 // CORS setup
-const allowlist = process.env.ALLOWLIST.split(',');
-const corsOptionsDelegate = function (req, callback) {
-    const corsOptions = allowlist.includes(req.header('Origin'))
-        ? { origin: true } // Allow listed origins
-        : { origin: false }; // Block other origins
+const allowlist = list.split(',');
+const corsOptionsDelegate = (req, callback) => {
+    const origin = req.header('Origin');
+    const corsOptions = allowlist.includes(origin)
+        ? { origin: true }
+        : { origin: false };
+    console.log(`Origin: ${origin} - Allowed: ${corsOptions.origin}`);
     callback(null, corsOptions);
-}
+};
 
 app.use(cors(corsOptionsDelegate));
 
