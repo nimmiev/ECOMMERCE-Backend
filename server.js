f13b2cdb58d6f23863de24a3f31dab6b03b709c2
nimmiev <nimmiev222@gmail.com>
2025-01-20 17:29:33 +0530

    update cors

--- server.js
+++ server.js
@@ -4,22 +4,23 @@ const mongoose = require('mongoose')
 const app = express()
 const productRouter = require('./src/routes/productRouter')
 const userRouter = require('./src/routes/userRouter')
+const cartRouter = require('./src/routes/cartRouter')
 const ProductModel = require('./src/models/productModel')
 const port = process.env.PORT || 3000
 const cors = require('cors')
 
 async function main() {
-    await mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    await mongoose.connect(process.env.MONGO_URI)
 }
 
 main()
 .then(() => {
     console.log("DB connected..")
 })
-.catch(err => console.log(err))
+.catch(err => console.log(err));
 
 // CORS setup
-const allowlist = process.env.ALLOWLIST.split(',');
+const allowlist = ['http://localhost:5173','http://127.0.0.1:5173','https://ecommerce-backend-rho.vercel.app/'];
 const corsOptionsDelegate = function (req, callback) {
     const corsOptions = allowlist.includes(req.header('Origin'))
         ? { origin: true } // Allow listed origins
@@ -32,6 +33,7 @@ app.use(cors(corsOptionsDelegate));
 app.use(express.json())
 app.use("/", productRouter);
 app.use("/user", userRouter);
+app.use("/cart", cartRouter);
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
